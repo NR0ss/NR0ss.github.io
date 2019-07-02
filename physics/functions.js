cfdc9f6336db69ca4035b9e7b86ec0c3f53197c7
Nathan Rossbach <13rossbachn@gmail.com>
2019-07-02 18:05:19 +0100

    updated

--- physics/functions.js
+++ physics/functions.js
@@ -1,6 +1,6 @@
 var script = document.createElement('script');
  
-script.src = '//code.jquery.com/jquery-1.11.0.min.js';
+script.src = 'https://code.jquery.com/jquery-1.12.4.min.js';
 document.getElementsByTagName('head')[0].appendChild(script); 
 
 var loadedTopics = ""
@@ -8,15 +8,14 @@ var loadedTopics = ""
 function loadTopic(name, idOfButton, divID) {
 	$(document).ready(function(){
 		
-		name = "#" + name
-		idOfButton = "#" + idOfButton
-		divID = "#" + divID
+		name = "#" + name;
+		idOfButton = "#" + idOfButton;
+		divID = "#" + divID;
 		
-		$(idOfButton).click(function(){
 			if (loadedTopics.includes(name) == false) {
 				$(divID).load("./Topics.html " + name);
 				loadedTopics = name + " " +  loadedTopics;
 			}
-		});
 	});
 }
+
