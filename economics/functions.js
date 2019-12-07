f2a7c1bcfe72e9f45a9d5e195f8deff9449c655b
BuildTools <13rossbachn@gmail.com>
2019-12-07 13:43:10 +0000

    fixed function

--- economics/functions.js
+++ economics/functions.js
@@ -3,7 +3,7 @@ var script = document.createElement('script');
 script.src = 'https://code.jquery.com/jquery-1.12.4.min.js';
 document.getElementsByTagName('head')[0].appendChild(script); 
 
-var loadedTopics = ""
+var loadedTopics = "";
 
 function loadTopic(name, idOfButton, divID) {
 	$(document).ready(function(){
